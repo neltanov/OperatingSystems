02c8e1c4c1e066048a53f638bdab357ac9944888
Bayarto Neltanov <bayarto2003@gmail.com>
2023-03-28 01:26:14 +0700

    add build.sh

---
+++ lab3-2/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+gcc main.c -o main
+ln main bin/mk_dir
+ln main bin/show_files
+ln main bin/rm_dir
+ln main bin/create_file
+ln main bin/show_file_content
+ln main bin/rm_file
+ln main bin/create_sym_link
+ln main bin/show_sym_link_content
+ln main bin/show_sym_linked_file_content
+ln main bin/rm_sym_link
+ln main bin/create_hard_link
+ln main bin/rm_hard_link
+ln main bin/show_mod_and_nlink
+ln main bin/change_access_permissions
