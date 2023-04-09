6eb3128784eca98dbcefd1493549736ddf6e8981
Bayarto Neltanov <bayarto2003@gmail.com>
2023-04-09 23:52:07 +0700

    add pagemap.c

--- lab3-3/pagemap.c
+++ lab3-3/pagemap.c
@@ -1,98 +1,60 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <assert.h>
+#include <fcntl.h>
 #include <errno.h>
-#include <stdint.h>
 
-#define PAGEMAP_ENTRY 8
-#define GET_BIT(X,Y) (X & ((uint64_t)1<<Y)) >> Y
-#define GET_PFN(X) X & 0x7FFFFFFFFFFFFF
+#define PAGE_SIZE 0x1000
 
-const int __endian_bit = 1;
-#define is_bigendian() ( (*(char*)&__endian_bit) == 0 )
-
-int i, c, pid, status;
-unsigned long virt_addr;
-uint64_t read_val, file_offset;
-char path_buf [0x100] = {};
-FILE * f;
-char *end;
-
-int read_pagemap(char * path_buf, unsigned long virt_addr);
-
-int main(int argc, char ** argv){
-    //printf("%lu\n", GET_BIT(0xA680000000000000, 63));
-    //return 0;
-    if(argc!=3){
-        printf("Argument number is not correct!\n pagemap PID VIRTUAL_ADDRESS\n");
-        return -1;
-    }
-    if(!memcmp(argv[1],"self",sizeof("self"))){
-        sprintf(path_buf, "/proc/self/pagemap");
-        pid = -1;
-    }
-    else{
-        pid = strtol(argv[1],&end, 10);
-        if (end == argv[1] || *end != '\0' || pid<=0){
-            printf("PID must be a positive number or 'self'\n");
-            return -1;
-        }
-    }
-    virt_addr = strtol(argv[2], NULL, 16);
-    if(pid!=-1)
-        sprintf(path_buf, "/proc/%u/pagemap", pid);
-
-    read_pagemap(path_buf, virt_addr);
-    return 0;
+static void print_page(uint64_t address, uint64_t data) {
+    printf("0x%-16lx : pfn %-16lx soft-dirty %ld file/shared %ld "
+           "swapped %ld present %ld\n",
+           address,
+           data & 0x7fffffffffffff,
+           (data >> 55) & 1,
+           (data >> 61) & 1,
+           (data >> 62) & 1,
+           (data >> 63) & 1);
 }
 
-int read_pagemap(char * path_buf, unsigned long virt_addr){
-    printf("Big endian? %d\n", is_bigendian());
-    f = fopen(path_buf, "rb");
-    if(!f){
-        printf("Error! Cannot open %s\n", path_buf);
-        return -1;
+int main(int argc, char *argv[]) {
+    char filename[BUFSIZ];
+    if (argc != 4) {
+        printf("Usage: %s pid start_address end_address\n",
+               argv[0]);
+        return 1;
     }
 
-    //Shifting by virt-addr-offset number of bytes
-    //and multiplying by the size of an address (the size of an entry in pagemap file)
-    file_offset = virt_addr / getpagesize() * PAGEMAP_ENTRY;
-    printf("Vaddr: 0x%lx, Page_size: %d, Entry_size: %d\n", virt_addr, getpagesize(), PAGEMAP_ENTRY);
-    printf("Reading %s at 0x%llx\n", path_buf, (unsigned long long) file_offset);
-    status = fseek(f, file_offset, SEEK_SET);
-    if(status){
-        perror("Failed to do fseek!");
-        return -1;
-    }
     errno = 0;
-    read_val = 0;
-    unsigned char c_buf[PAGEMAP_ENTRY];
-    for(i=0; i < PAGEMAP_ENTRY; i++){
-        c = getc(f);
-        if(c==EOF){
-            printf("\nReached end of the file\n");
-            return 0;
+    int pid = (int) strtol(argv[1], NULL, 0);
+    if (errno) {
+        perror("strtol");
+        return 1;
+    }
+    snprintf(filename, sizeof filename, "/proc/%d/pagemap", pid);
+
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    uint64_t start_address = strtoul(argv[2], NULL, 0);
+    uint64_t end_address = strtoul(argv[3], NULL, 0);
+    printf("%lu\n", start_address);
+    printf("%lu\n", end_address);
+
+    for (uint64_t i = start_address; i < end_address; i += 0x1000) {
+        uint64_t data;
+        uint64_t index = (i / PAGE_SIZE) * sizeof(data);
+        if (pread(fd, &data, sizeof(data), index) != sizeof(data)) {
+            perror("pread");
+            break;
         }
-        if(is_bigendian())
-            c_buf[i] = c;
-        else
-            c_buf[PAGEMAP_ENTRY - i - 1] = c;
-        printf("[%d]0x%x ", i, c);
+        print_page(i, data);
     }
-    for(i=0; i < PAGEMAP_ENTRY; i++){
-        //printf("%d ",c_buf[i]);
-        read_val = (read_val << 8) + c_buf[i];
-    }
-    printf("\n");
-    printf("Result: 0x%llx\n", (unsigned long long) read_val);
-    //if(GET_BIT(read_val, 63))
-    if(GET_BIT(read_val, 63))
-        printf("PFN: 0x%llx\n",(unsigned long long) GET_PFN(read_val));
-    else
-        printf("Page not present\n");
-    if(GET_BIT(read_val, 62))
-        printf("Page swapped\n");
-    fclose(f);
+
+    close(fd);
     return 0;
 }
