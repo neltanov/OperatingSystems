214303d5cec653c807378f6668b65fb42532a32f
Bayarto Neltanov <bayarto2003@gmail.com>
2023-04-22 22:29:21 +0700

    Done some points of task

--- lab4-1/vadr.c
+++ lab4-1/vadr.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-int global_init = 0;
+#define BUF_SIZE 100
+
+int global_init = 2;
 int global_not_init;
 
-void init_local() {
+int *init_local() {
     int local;
     printf("Local in function not initialized: %p\n", &local);
     local = 2;
     printf("Local in function initialized: %p\n", &local);
+    return &local;
 }
 
-void print_variables() {
+void work_with_buf() {
+    char *buf1 = malloc(BUF_SIZE * sizeof(char));
+    strcpy(buf1, "hello, world1");
+    printf("%s\n", buf1);
+    free(buf1);
+    printf("%s\n", buf1);
+
+    char *buf2 = malloc(BUF_SIZE * sizeof(char));
+    strcpy(buf2, "hello, world2");
+    printf("%s\n", buf2);
+    free(buf2 + BUF_SIZE / 2);
+    printf("%s\n", buf2);
+}
+
+void print_var_adresses() {
     int local;
     static int static_local;
     const int const_local;
@@ -21,13 +40,14 @@ void print_variables() {
            "Global initialized: %p\n"
            "Global not initialized: %p\n"
            "Function: %p\n",
-           &local, &static_local, &const_local, &global_init, &global_not_init, &print_variables);
-    init_local();
+           &local, &static_local, &const_local, &global_init, &global_not_init, &print_var_adresses);
+    printf("Local in function: %p\n", init_local());
 }
 
 int main() {
     printf("Pid: %d\n", getpid());
-    print_variables();
+    print_var_adresses();
+    work_with_buf();
     sleep(30);
     return 0;
 }
